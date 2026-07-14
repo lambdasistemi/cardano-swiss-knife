959ea87649606ace4f11435f6b8e0d40c311a58c
paolino <paolo.veronelli@gmail.com>
2026-07-14 12:21:33 +0100

    chore: extend gate.sh with inspector workbench build

--- gate.sh
+++ gate.sh
@@ -7,6 +7,7 @@ nix run .#ci-check
 nix run .#ci-haskell-quality
 nix run .#ci-check-vectors
 nix run .#ci-build
+nix build .#tx-inspector-ui --no-link
 nix develop github:paolino/dev-assets?dir=mkdocs --quiet -c mkdocs build --strict
 nix run .#ci-test
 nix run .#ci-playwright
